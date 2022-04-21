8ee4ba4a8109cfab7f0b29c5c185fc6d579f332d
maginkaaa <57997851+maginkaaa@users.noreply.github.com>
2022-04-21 14:07:18 +0200

    added simple routes

--- sprint1_connection/main.js
+++ sprint1_connection/main.js
@@ -1,24 +1,34 @@
-const routeResponseMap = {
-    "/info": "<h1>Info Page</h1>",
-    "/contact": "<h1>Contact Us</h1>",
-    "/about": "<h1>Learn More About Us.</h1>",
-    "/hello": "<h1>Say hello by emailing us here</h1>",
-    "/error": "<h1>Sorry the page you are looking for is not here.</h1>"
-   };
-
-   const port = 3000,
-   http = require("http"),
-   httpStatus = require("http-status-codes"),
-   app = http.createServer((req, res) => {
-   res.writeHead(200, {
-  "Content-Type": "text/html"
-   });
-   if (routeResponseMap[req.url]) {
-  res.end(routeResponseMap[req.url]);
-   } else {
-       res.end("<h1>Welcome!</h1>");
+const port = 3000,
+ http = require("http"),
+ httpStatusCodes = require("http-status-codes"),
+ router = require("./router"),
+ fs = require("fs"),
+ plainTextContentType = {
+ "Content-Type": "text/plain"
+ },
+ htmlContentType = {
+ "Content-Type": "text/html"
+ },
+ customReadFile = (file, res) => {
+ fs.readFile(`./${file}`, (errors, data) => {
+if (errors) {
+console.log("Error reading the file...");
 }
+res.end(data);
+ });
+ };
+router.get("/", (req, res) => {
+ res.writeHead(httpStatusCodes.OK, plainTextContentType);
+ res.end("INDEX");
 });
-app.listen(port);
-console.log(`The server has started and is listening on port number:
+router.get("/index.html", (req, res) => {
+ res.writeHead(httpStatusCodes.OK, htmlContentType);
+ customReadFile("views/index.html", res);
+});
+router.post("/", (req, res) => {
+ res.writeHead(httpStatusCodes.OK, plainTextContentType);
+ res.end("POSTED");
+});
+http.createServer(router.handle).listen(3000);
+console.log(`The server is listening on port number:
 ➥ ${port}`);
